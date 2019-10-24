cfd18c6128985a954249bd409e3ce677be0fb745
haldny <haldny@motorola.com>
2019-10-24 10:03:45 -0300

    Aula 3, Adicao de Intents e Intens Filters

--- app/src/main/java/com/haldny/projetoaula1/MainActivity.kt
+++ app/src/main/java/com/haldny/projetoaula1/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.haldny.projetoaula1
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
@@ -34,6 +38,62 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity3::class.java)
 
             startActivityForResult(intent, REQUEST_CODE)
+        }
+
+        val button3 = findViewById<Button>(R.id.activity1_button3)
+        button3.setOnClickListener {
+            val uri = Uri.parse("https://www.google.com.br")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            startActivity(intent)
+        }
+
+        val button4 = findViewById<Button>(R.id.activity1_button4)
+        button4.setOnClickListener {
+
+            val hasPermission = checkSelfPermission(Manifest.permission.CALL_PHONE)
+
+            if (hasPermission == PackageManager.PERMISSION_GRANTED) {
+                val uri = Uri.parse("tel:819885567877")
+                val intent = Intent(Intent.ACTION_CALL, uri)
+                startActivity(intent)
+            } else {
+                val permissions = arrayOf(Manifest.permission.CALL_PHONE)
+                requestPermissions(permissions, 100)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 100
+            && permissions[0] == Manifest.permission.CALL_PHONE
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val uri = Uri.parse("tel:819885567877")
+            val intent = Intent(Intent.ACTION_CALL, uri)
+
+            if (checkSelfPermission(Manifest.permission.CALL_PHONE)
+                == PackageManager.PERMISSION_GRANTED) {
+                startActivity(intent)
+            }
+        } else {
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("Android Básico")
+
+            builder.setCancelable(false)
+            builder.setMessage("Hoje foi aula de Intents e Intents filters.")
+
+            builder.setPositiveButton("FOI MASSA", { dialog, _ ->
+                dialog.dismiss()
+            })
+
+            val dialog = builder.create()
+
+            dialog.show()
         }
     }
 
